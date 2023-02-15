f53b9d374bd4df94e42869e3497eea83414e466e
njoudCs <njoud.cs.pnu@gmail.com>
2023-02-15 16:47:48 +0300

    Add 2 views

---
+++ let'sGo.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+
+struct let_sGo: View {
+    var body: some View {
+        
+    }
+}
+
+struct let_sGo_Previews: PreviewProvider {
+    static var previews: some View {
+        let_sGo()
+    }
+}
