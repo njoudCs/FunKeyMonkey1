9ffe8b41fd08891c75281c870f062e75a2c20086
Monirah90 <monirah_aljarrah@hotmail.com>
2023-02-16 15:52:42 +0300

    add first page

--- FirstlyPage.swift
+++ FirstlyPage.swift
@@ -130,24 +130,6 @@ struct FirstlyPage: View {
 //End function
   }
 
-struct StrokeText: View {
-let text: String
-let width: CGFloat
-let color: Color
-
-var body: some View {
-    ZStack{
-        ZStack{
-            Text(text).offset(x:  width, y:  width)
-            Text(text).offset(x: -width, y: -width)
-            Text(text).offset(x: -width, y:  width)
-            Text(text).offset(x:  width, y: -width)
-        }
-        .foregroundColor(color)
-        Text(text)
-    }
-}
-}
 
 
 
