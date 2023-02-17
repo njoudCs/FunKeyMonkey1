60f833d72ffdbfa0b14b686a47159a19d2aa672c
Lujain <lujain_mohammed@outlook.sa>
2023-02-17 23:28:31 +0300

    ready?

--- ready?.swift
+++ ready?.swift
@@ -1,13 +1,32 @@
 import SwiftUI
 
 struct ready_: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    HStack{
+      Image("monkey")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 400)      
+      VStack{
+        Text("Ready?")
+        Text("go")
+      }
+      ZStack{
+        Rectangle()
+          .foregroundColor(Color("Label"))
+          .frame(width: 300, height: 348)
+        VStack{
+          Text("Stand up")
+          Text("here")
+        }
+      }
     }
-}
-
-struct ready__Previews: PreviewProvider {
+  }
+  
+  struct ready__Previews: PreviewProvider {
     static var previews: some View {
-        ready_()
+      ready_()
     }
+  }
+  
 }
